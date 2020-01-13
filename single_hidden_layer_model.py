42ec55a32bee0f58455d18c14d34cd6285bbc268
JayWu7 <jaywu16@163.com>
2020-01-13 11:53:21 +0200

    update gitignore file

---
+++ single_hidden_layer_model.py
@@ -0,0 +1,292 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from os import listdir
+from PIL import Image
+import shutil
+import scipy.linalg.blas as blas
+
+
+# def processing_img(directory='./dataset/train/sushi/'):
+#     res = []
+#     for im_name in listdir(directory):
+#         if not im_name.startswith('.'):  # ignore the hidden file in the directory
+#             filename = directory + im_name
+#             im = Image.open(filename)
+#             if im.size == (300, 300):
+#                 shutil.copy(filename, './dataset/train/data')
+#
+#
+# processing_img()
+
+
+# upload the data from dataset
+def upload_img_data(directory='./dataset/train/data'):
+    '''
+    read the images and return the matrix of these images
+    :param directory: directory which store these images
+    :return:  numpy arrays with shape(m, 300,300,3)
+    '''
+    images_name = [n for n in listdir(directory) if not n.startswith('.')]
+    images = []
+    labels = []
+    for na in images_name:
+        im = plt.imread('{}/{}'.format(directory, na), 'JPG')
+        images.append(im)
+        if na.startswith('sushi'):  # 0 means this pic is a sushi pic, 1 means sandwich
+            labels.append(0)
+        else:
+            labels.append(1)
+    images = np.array(images, dtype=np.float32)  # transfer the list type to numpy ndarray
+    return images, labels
+
+
+def processing_image_matrix(images, labels):
+    '''
+    using reduction and normalization to process our data.
+    :param images: numpy array, store the matrix of images
+    :return: numpy array, with shape(300*300*3, m)
+    '''
+    images_flatten = images.reshape(images.shape[0], -1).T
+    # normalize data
+    train_set_x = images_flatten / 255
+    train_set_y = np.array(labels, dtype=np.float32).reshape((1, len(labels)))
+    return train_set_x, train_set_y
+
+
+# def upload_img_data(directory='./dataset/train/data'):
+#     '''
+#     read the images and return the matrix of these images
+#     :param directory: directory which store these images
+#     :return:  numpy arrays with shape(m, 300,300,3)
+#     '''
+#     images_name = [n for n in listdir(directory) if not n.startswith('.')]
+#     images = np.ndarray((len(images_name), 300, 300, 3))
+#     for i, na in enumerate(images_name):
+#         images[i] = plt.imread('{}/{}'.format(directory, na), 'JPG')
+#     print(images.shape)
+#     return images
+
+
+def sigmoid(z):
+    '''
+    implementing sigmoid function
+    :param z: numpy array of scalar
+    :return: sigmoid(z)
+    '''
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+
+# define the neural network structure
+def layer_sizes(X):
+    '''
+    define the size of neural network layer
+    :param X: input dataset of shape(input size, number of samples)
+    :return:
+    n_x -- the size of input layer
+    n_h -- the size of hidden layer
+    n_y -- the size of output layer
+    '''
+    n_x = X.shape[0]
+    n_h = 5  # set the hidden number
+    n_y = 1
+    return n_x, n_h, n_y
+
+
+# initialize the model's parameters
+def initialize_parameters(n_x, n_h, n_y):
+    '''
+    randomly initialize the parameters of w and b.
+    :param n_x: size of input layer
+    :param n_h: size of hidden layer
+    :param n_y: size of output layer
+    :return: params -- Python dictionary containing our parameters:
+                            W1 -- weight matrix of shape (n_h, n_x)
+                            b1 -- bias vector of shape (n_h, 1)
+                            W2 -- weight matrix of shape (n_y, n_h)
+                            b2 -- bias vector of shape(n_y, 1)
+    '''
+    np.random.seed(16)  # set random seed for the convenience of reproduce
+
+    W1 = np.random.randn(n_h, n_x).astype(dtype='float32')
+    b1 = np.zeros((n_h, 1)).astype(dtype='float32')
+    W2 = np.random.randn(n_y, n_h).astype(dtype='float32')
+    b2 = np.zeros((n_y, 1)).astype(dtype='float32')
+
+    # using assert statement to make sure our parameters are in the correct shape
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+
+    parameters = {
+        'W1': W1,
+        'b1': b1,
+        'W2': W2,
+        'b2': b2
+    }
+
+    return parameters
+
+
+# Implement the forward_propagation function:
+
+def forward_propagation(X, parameters):
+    '''
+    implement forward propagation of neural network
+    :param X: input data of shape (n_x, m)
+    :param parameters: a python dict including our parameters
+    :return:
+            A2 -- The sigmoid output of the second activation
+            cache -- a dictionary containing 'Z1', 'A1', 'Z2' and 'A2'
+    '''
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+
+    # Z1 = np.dot(W1, X) + b1
+    Z1 = blas.sgemm(alpha=1.0, a=W1, b=X) + b1
+    A1 = np.tanh(Z1)
+    # Z2 = np.dot(W2, A1) + b2
+    Z2 = blas.sgemm(alpha=1.0, a=W2, b=A1) + b2
+    A2 = sigmoid(Z2)
+
+    assert A2.shape == (1, X.shape[1])
+
+    cache = {
+        'Z1': Z1,
+        'A1': A1,
+        'Z2': Z2,
+        'A2': A2
+    }
+
+    return A2, cache
+
+
+# Implement cost function
+def compute_cost(A2, Y, parameters):
+    '''
+    Compute the cross-entropy cost
+    :param A2: The sigmoid output of the second activation, of shape (1, m)
+    :param Y: 'true' labels vector of shape (1, m)
+    :param parameters: Python dict containing our parameters
+    :return:
+            cost -- cross-entropy cost given equation
+    '''
+    m = Y.shape[1]
+    logprobs = np.multiply(np.log(A2), Y) + np.multiply(1 - Y, np.log(1 - A2))
+    cost = -np.sum(logprobs)
+    cost = float(np.squeeze(cost)) / m
+    assert isinstance(cost, float)
+
+    return cost
+
+
+# Implement Backward propagation
+def backward_propagation(parameters, cache, X, Y):
+    '''
+    Implement the backward propagation
+    :param parameters: python dict containing our parameters
+    :param cache: python dict containing 'Z1', 'A1', 'Z2', 'A2' we got above
+    :param X: input data of shape(2, number of examples)
+    :param Y: 'true' labels vector of shape (1, number of examples)
+    :return:
+            grads -- python dict including our gradients with respect to different parameters.
+    '''
+
+    m = X.shape[1]
+    # W1 = parameters['W1']
+    W2 = parameters['W2']
+
+    A1 = cache['A1']
+    A2 = cache['A2']
+
+    dZ2 = A2 - Y
+    dW2 = np.dot(dZ2, A1.T) / m
+    # dW2 = blas.sgemm(alpha=1.0, a=dZ2, b=A1.T) / m
+    db2 = np.sum(dZ2, axis=1, keepdims=True) / m
+    dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1, 2))
+    # dZ1 = blas.sgemm(alpha=1.0, a=W2.T, b=dZ2) * (1 - np.power(A1, 2))
+    dW1 = np.dot(dZ1, X.T) / m
+    # dW1 = blas.sgemm(alpha=1.0, a=dZ1, b=X.T) / m
+    db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+
+    return grads
+
+
+# using gradient decent to update our parameters
+
+def update_parameters(parameters, grads, learning_rate=1.1):
+    '''
+    Updates parameters using the gradient descent
+    :param parameters: python dictionary containing our parameters
+    :param grads: python dictionary containing our gradients
+    :param learning_rate:  learning rate of gradient decent
+    :return:
+            parameters -- python dict including our updated parameters
+    '''
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+
+    dW1 = grads['dW1']
+    db1 = grads['db1']
+    dW2 = grads['dW2']
+    db2 = grads['db2']
+
+    W1 = W1 - learning_rate * dW1
+    W2 = W2 - learning_rate * dW2
+    b1 = b1 - learning_rate * db1
+    b2 = b2 - learning_rate * db2
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
+
+
+def nn_model(X, Y, n_h, num_iterations=10000, print_cost=False):
+    '''
+    our neural network model, combing all the function above together
+    :param X: training samples
+    :param Y: labels
+    :param n_h: size of hidden layer
+    :param num_iterations: Number of iterations in gradient descent loop
+    :param print_cost: if True, print the cost every 1000 iterations
+    :return:
+            parameters -- parameters learnt by the model
+    '''
+    np.random.seed(3)
+
+    n_x = layer_sizes(X)[0]
+    n_y = layer_sizes(X)[2]
+
+    params = initialize_parameters(n_x, n_h, n_y)
+
+    # Loop gradient descent
+    for i in range(num_iterations):
+        A2, cache = forward_propagation(X, params)
+        cost = compute_cost(A2, Y, params)
+        grads = backward_propagation(params, cache, X, Y)
+        params = update_parameters(params, grads)
+
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+
+    return params
+
+
+x, y = upload_img_data()
+X, Y = processing_image_matrix(x, y)
+
+parameters = nn_model(X, Y, 5, num_iterations=10000, print_cost=True)
